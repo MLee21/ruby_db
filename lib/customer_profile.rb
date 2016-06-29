f948bc771ef060ebd8911a3c58b1490ba5551ba2
MLee21 <minnie.lee89@gmail.com>
2016-06-29 14:57:11 -0600

    can parse files based off comma delimited or pipe delimited which will then return a hash of customer attributes

--- lib/customer_profile.rb
+++ lib/customer_profile.rb
@@ -1,5 +1,7 @@
 class CustomerProfile
 
+  ARRAY = %w(id last_name first_name gender favorite_color date_of_birth)
+
   attr_accessor :id,
                 :last_name,
                 :first_name,
@@ -10,8 +12,12 @@ class CustomerProfile
 
   def initialize(data)
     @data = delimiter_splitter(data)
-    require 'pry'
-    binding.pry;
+    @id = create_hash_attributes[:id].to_i
+    @last_name = create_hash_attributes[:last_name]
+    @first_name = create_hash_attributes[:first_name]
+    @gender = create_hash_attributes[:gender]
+    @favorite_color = create_hash_attributes[:favorite_color]
+    @date_of_birth = create_hash_attributes[:date_of_birth]
   end
 
   def delimiter_splitter(data)
@@ -20,6 +26,10 @@ class CustomerProfile
     else
       data.split("|")
     end
+  end
+
+  def create_hash_attributes
+    Hash[ARRAY.map(&:to_sym).zip(data)]
   end
 
 end
--- spec/customer_profile_spec.rb
+++ spec/customer_profile_spec.rb
@@ -7,7 +7,7 @@ describe CustomerProfile do
 
     context "when there is a new customer" do 
 
-      it "should create an array based on the delimiter" do 
+      it "should create an array based on the delimiter: comma" do 
         info = "1,Washington,Brenda,Female,Violet,10/18/1977"
         @customer = CustomerProfile.new(info)
         expect(@customer.data).to eq(["1", "Washington", "Brenda", "Female", "Violet", "10/18/1977"])
@@ -16,16 +16,26 @@ describe CustomerProfile do
         expect(@customer.data).to eq(["1", "Matthews", "Andrew", "Male", "Khaki", "07/24/1987"])
       end
 
-
-      it "should create a customer object with hash attributes" do 
+      it "should create a customer object with hash attributes from comma delimited" do 
         info = "1,Washington,Brenda,Female,Violet,10/18/1977"
         @customer = CustomerProfile.new(info)
         expect(@customer.id).to eq(1)
-        expect(@customer.last_name).to eq("Fox")
-        expect(@customer.first_name).to eq("Heather")
+        expect(@customer.last_name).to eq("Washington")
+        expect(@customer.first_name).to eq("Brenda")
         expect(@customer.gender).to eq("Female")
-        expect(@customer.favorite_color).to eq("Turquoise")
-        expect(@customer.date_of_birth).to eq(Time.new(customer.date_of_birth).strftime("%m/%d/%Y"))
+        expect(@customer.favorite_color).to eq("Violet")
+        expect(@customer.date_of_birth).to eq("10/18/1977")
+      end
+
+      it "should create a customer object with hash attributes from pipe delimited" do 
+        info = "1|Matthews|Andrew|Male|Khaki|07/24/1987"
+        @customer = CustomerProfile.new(info)
+        expect(@customer.id).to eq(1)
+        expect(@customer.last_name).to eq("Matthews")
+        expect(@customer.first_name).to eq("Andrew")
+        expect(@customer.gender).to eq("Male")
+        expect(@customer.favorite_color).to eq("Khaki")
+        expect(@customer.date_of_birth).to eq("07/24/1987")
       end
     end
   end
