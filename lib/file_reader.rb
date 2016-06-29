67639ea20a08d73f34af87b7b3233f07f175f4fe
MLee21 <minnie.lee89@gmail.com>
2016-06-28 21:59:53 -0600

    added test dummy data with comma delimited and pipe delimited examples. wrote tests to test for parsing of both types of files with different delimiters.

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,4 +1,17 @@
+require 'csv'
+
 class FileReader
 
+  attr_accessor :file, :csv
+
+  def initialize(file)
+    @file = file
+    @csv = []
+  end
+
+  def parse
+    File.foreach(file, headers: true) { |row| csv << row.strip }
+    csv
+  end
 
 end
--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,8 +1,49 @@
 require "file_reader"
 
-describe FileReader do 
 
-  it "runs" do 
-    expect(true).to eq(true)
+describe FileReader do 
+  attr_accessor :file_reader
+
+
+  describe "#parse_file" do 
+
+    context "when there is a comma delimited file" do 
+      before(:each) do 
+        test_data = 'customer_data_test_data.csv'
+        @file_reader = FileReader.new(test_data)
+      end
+
+      it "should have an empty csv array" do 
+        expect(file_reader.csv).to eq([])
+      end
+
+      it "should return the list of csv data" do 
+        expect(file_reader.parse.length).to eq(50)
+      end
+
+      it "should return a row of data that has no trailing whitespaces, new lines, or carriage returns" do 
+        expect(file_reader.parse.first).to eq("id,LastName,FirstName,Gender,FavoriteColor,DateOfBirth")
+      end
+    end
+
+    context "when there is a pipe delimited file" do 
+      before(:each) do 
+        pipe_delimited_test_data = 'customer_data_pipe_delimited test.txt'
+        @file_reader = FileReader.new(pipe_delimited_test_data)
+      end
+
+      it "should have any empty csv array" do 
+        expect(file_reader.csv).to eq([])
+      end
+
+      it "should return the list of txt data" do 
+        expect(file_reader.parse.length).to eq(51)
+      end
+
+      it "should return a row of data that has no trailing whitespaces, new lines, or carriage returns" do 
+        expect(file_reader.parse.first).to eq("id|LastName|FirstName|Gender|FavoriteColor|DateOfBirth")
+      end
+    end
+
   end
 end
