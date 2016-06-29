2432b44e2e937773ef2548050cfdce0ccc0438b8
MLee21 <minnie.lee89@gmail.com>
2016-06-28 20:11:54 -0600

    configured rspec. made sure that it is working.

---
+++ file_reader_spec.rb
@@ -0,0 +1,8 @@
+require "file_reader"
+
+describe FileReader do 
+
+  describe "rspec is running" do 
+    expect(true).to eq(true)
+  end
+end
---
+++ spec/file_reader_spec.rb
@@ -0,0 +1,8 @@
+require "file_reader"
+
+describe FileReader do 
+
+  it "runs" do 
+    expect(true).to eq(true)
+  end
+end
