1b00a1ea91bceb81d0ba88d01228a61a58a50002
MLee21 <minnie.lee89@gmail.com>
2016-06-30 13:47:47 -0600

    can query by last name, dob, and gender.

--- lib/data_retriever.rb
+++ lib/data_retriever.rb
@@ -11,4 +11,20 @@ class DataRetriever
     @customer_record_builder.create_customer_records
   end
 
+  def sort_by_gender
+    all.sort_by { |k,v| k.gender }
+  end
+
+  def sort_by_last_name_ascending
+    all.sort_by { |k,v| k.last_name }
+  end
+
+  def sort_by_dob_ascending
+    all.map.sort_by { |k,v| Date.strptime(k.date_of_birth, "%m/%d/%Y") }
+  end
+
+  def sort_by_last_name_descending
+    sort_by_last_name_ascending.reverse
+  end
+
 end
--- spec/data_retriever_spec.rb
+++ spec/data_retriever_spec.rb
@@ -12,6 +12,26 @@ describe DataRetriever do
       it "should return all of the records" do 
         expect(@data_retriever.all.count).to eq(50)
       end
+
+      it "should return records sorted by gender (f before m)" do 
+        expect(@data_retriever.sort_by_gender.first.gender).to eq('Female')
+        expect(@data_retriever.sort_by_gender.last.gender).to eq('Male')
+      end
+
+      it "should return records by last name ascending" do 
+        expect(@data_retriever.sort_by_last_name_ascending.first.last_name).to eq("Alvarez")
+        expect(@data_retriever.sort_by_last_name_ascending.last.last_name).to eq("Williams")
+      end
+
+      it "should return records by date of birth ascending" do 
+        expect(@data_retriever.sort_by_dob_ascending.first.date_of_birth).to eq("12/20/1970")
+        expect(@data_retriever.sort_by_dob_ascending.last.date_of_birth).to eq("06/10/1996")
+      end
+
+      it "should return records by last name descending" do 
+        expect(@data_retriever.sort_by_last_name_descending.first.last_name).to eq("Williams")
+        expect(@data_retriever.sort_by_last_name_descending.last.last_name).to eq("Alvarez")
+      end
     end
   end
 end
